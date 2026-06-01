b65f8c489fcfd4aff08931c0b5ee6a08c7f142df
sundial-shivansh <shivansh@sundial.so>
2026-06-01 14:07:06 +0530

    Trimmed

--- sundial_airflow/backfill/__init__.py
+++ sundial_airflow/backfill/__init__.py
@@ -1,25 +1,4 @@
-"""Lineage-aware chunked backfill DAG framework. Tenant DAGs call
-:func:`make_backfill_dag`; other names are exported for tests and tooling."""
+"""Lineage-aware chunked backfill DAG framework. Tenant DAGs call :func:`make_backfill_dag`."""
 from sundial_airflow.backfill.dag_factory import make_backfill_dag
-from sundial_airflow.backfill.manifest_parser import (
-    CHUNKED,
-    FULL_REFRESH,
-    BackfillModel,
-    ChunkingConfigEntry,
-    compute_static_chunks,
-    load_backfill_models,
-    load_chunking_config,
-    topological_order,
-)
 
-__all__ = [
-    "make_backfill_dag",
-    "BackfillModel",
-    "ChunkingConfigEntry",
-    "CHUNKED",
-    "FULL_REFRESH",
-    "load_chunking_config",
-    "load_backfill_models",
-    "topological_order",
-    "compute_static_chunks",
-]
+__all__ = ["make_backfill_dag"]
